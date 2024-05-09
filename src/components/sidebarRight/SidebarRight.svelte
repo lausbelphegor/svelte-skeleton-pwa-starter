4045f998e56da50c4a80d03e294fe31f7df6a1c4
lausbelphegor <lausbelphegor@gmail.com>
2024-05-09 11:28:13 +0300

    .

---
+++ src/components/sidebarRight/SidebarRight.svelte
@@ -0,0 +1,7 @@
+<script>
+</script>
+
+<div class="flex flex-row select-none">
+	<div class="basis-1/2">
+	</div>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,58 @@
+<script>
+	import '../app.postcss';
+	import { AppShell, AppBar, LightSwitch } from '@skeletonlabs/skeleton';
+
+	import SidebarRight from '../components/sidebarRight/SidebarRight.svelte';
+
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	import { onMount } from 'svelte';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+	async function detectSWUpdate() {
+		const registration = await navigator.serviceWorker.ready;
+
+		registration.addEventListener('updatefound', () => {
+			const newWorker = registration.installing;
+
+			newWorker?.addEventListener('statechange', () => {
+				if (newWorker.state === 'installed') {
+					if (confirm('A new version is available. Refresh to update?')) {
+						newWorker.postMessage({ type: 'SKIP_WAITING' });
+						window.location.reload();
+					}
+				}
+			});
+		});
+	}
+
+	onMount(() => {
+		detectSWUpdate();
+	});
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<div id="title-bar" slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-sm uppercase">Kema</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail"></svelte:fragment>
+		</AppBar>
+	</div>
+	<!-- Page Route Content -->
+	<svelte:fragment slot="sidebarRight">
+		<SidebarRight />
+	</svelte:fragment>
+
+	<slot />
+</AppShell>
+
+<style lang="postcss">
+	#title-bar {
+		-webkit-app-region: drag;
+	}
+</style>
